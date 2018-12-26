bb80f2098ebac8c2c75d9f7d9b84e1e03537c19d
WardSabha <46149209+WardSabha@users.noreply.github.com>
2018-12-26 16:19:00 +0800

    Delete Tabletop.js

--- Tabletop.js
+++
@@ -1,8 +0,0 @@
-function init() {
- Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/10QRvh9jL24iLknhlSXIJnvLCaBaSZpVwAI0hve1Khik/pubhtml',
- callback: function(data, tabletop) { 
- console.log(data)
- },
- simpleSheet: true } )
-}
-window.addEventListener(‘DOMContentLoaded’, init)
